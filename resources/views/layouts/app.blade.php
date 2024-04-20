bcbdfb4c5f8615b55a40933e7cc4b0e038763385
Girish Pareek <121708026+girish104@users.noreply.github.com>
2024-04-20 18:58:19 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,33 +36,41 @@
     @yield('styles')
 </head>
     
-<body
-    class="container text-white mx-auto mt-10 mb-10 max-w-lg bg-slate-100relative bg-cover bg-center bg-no-repeat bg-fixed"
-    style="background-image: url('/images/list.jpg')">
-    <div class="absolute inset-0 bg-black opacity-80"></div>
 
-    <div class="relative z-10 p-4 mx-auto mt-8 max-w-md bg-black rounded-lg bg-opacity-10 shadow-2xl">
-        <h1 class="text-2xl mb-6">
-            @yield('title')
-        </h1>
+<body class="container text-white mx-auto mt-10 mb-10 max-w-lg bg-slate-600 ">
 
-        <div x-data="{ flash: true }">
-            @if (session()->has('success'))
-                <div x-show = "flash"
-                    class=" relative mb-10 rounded border border-green-500 bg-green-100 px-4 py-3 text-green-700 text-lg"
-                    role="alert">
-                    <Strong class="font-bold">Success!</Strong>
-                    <div>{{ session('success') }}</div>
-                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            @click="flash = false" stroke="currentColor" class="h-6 w-6 cursor-pointer">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </span>
-                </div>
-            @endif
-            @yield('content')
+    <div class="relative p-4 mx-auto mt-8 max-w-md bg-cover bg-center bg-no-repeat rounded-lg bg-fixed">
+
+        {{-- <div class="absolute inset-0 bg-black opacity-70 rounded-lg z-0"></div> --}}
+
+        <div class="relative z-10">
+
+            <h1 class="text-2xl mb-6">
+                @yield('title')
+            </h1>
+
+            <div x-data="{ flash: true }">
+                @if (session()->has('success'))
+                    <div x-show = "flash"
+                        class=" relative mb-10 rounded border border-green-500 bg-green-100 px-4 py-3 text-green-700 text-lg"
+                        role="alert">
+                        <Strong class="font-bold">Success!</Strong>
+                        <div>{{ session('success') }}</div>
+                        <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" @click="flash = false" stroke="currentColor"
+                                class="h-6 w-6 cursor-pointer">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </span>
+                    </div>
+                @endif
+                @yield('content')
+            </div>
         </div>
+    </div>
+
 </body>
 
 </html>
+
